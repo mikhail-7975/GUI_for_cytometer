bda80780a288aa48b3fe1e6eb99d4610a8ed1258
Михаил Калиниченко <mikhail.kalinichenko.1999@gmail.com>
2020-02-28 09:53:55 +0700

    reading from port in class

--- GUI_for_citometr/module1.py
+++ GUI_for_citometr/module1.py
@@ -3,25 +3,35 @@ import serial
 s = serial.Serial('COM10')
 print(type(s))
 
-def readFromComPort(ComPort, #port from which data is read
-                    startSymbol, #symvol of beginning data binary string
-                    endSymbol #sumbo; of ending data, binary string
-                    ):
-    result = ""
-    inp = b''
-    while(inp != startSymbol): #skip data before start symbol
-        inp = s.read()
 
-    while(1):                  #read data from port until get endSymbol
-        inp = s.read()
-        if (inp == endSymbol):
-            break
-        result += inp.decode("utf-8")
+class ComPortRW:
+    port = srial.Serial('') #port from which data is read
+    startSymbol = '>' #symvol of beginning data binary string
+    endSymbol = '<' #sumbo; of ending data, binary string
+                        
 
-    return result
 
-def writeDataToPort(data):#, port):
-    print(data.encode('utf-8'))
+    def __init__(self, comPortName = str, startSymbol = str, endSymbol = str):
+        self.port = serial.Serial(comPortName)
+        self.startSymbol = startSymbol
+        self.endSymbol = endSymbol
+
+    def readFromComPort(self):
+        result = ""
+        inp = b''
+        while(inp != self.startSymbol): #skip data before start symbol
+            inp = s.read()
+
+        while(1):                  #read data from port until get endSymbol
+            inp = s.read()
+            if (inp == self.endSymbol):
+                break
+            result += inp.decode("utf-8")
+
+        return result
+
+    def writeToComPort(self, data):#, port):
+        print(data.encode('utf-8'))
 
 
 for i in range(10):
